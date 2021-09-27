8e2650da60e93d8439e42e8535feed4f74a60095
KwangtaekJung <kt79.jung@gmail.com>
2021-09-27 17:29:24 +0900

    스프링 시큐리티 커스터마이징: JPA 연동

--- src/main/java/com/example/inflearnspringsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/inflearnspringsecurity/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.inflearnspringsecurity.config;
 
+import com.example.inflearnspringsecurity.account.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,10 +12,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    AccountService accountService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/", "/info").permitAll()
+                .mvcMatchers("/", "/info", "/account/**").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
@@ -22,10 +27,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .httpBasic();
     }
 
+    // 이렇게 명시해주지 않아도 UserDetailsService 타입이 Bean으로 등록되어 있으면 알아서 가져가서 사용한다.
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("keesun").password("{noop}123").roles("USER").and()
-                .withUser("admin").password("{noop}!@#").roles("ADMIN");
+        auth.userDetailsService(accountService);
     }
+
+    //Account를 만들어서 사용할 것이기 때문에 더 이상 이부분은 필요 없다.
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("keesun").password("{noop}123").roles("USER").and()
+//                .withUser("admin").password("{noop}!@#").roles("ADMIN");
+//    }
 }
