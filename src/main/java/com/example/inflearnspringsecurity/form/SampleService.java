4b89fa6b9895c7b25394180a8fbc02a3cd2fe713
KwangtaekJung <kt79.jung@gmail.com>
2021-09-30 06:26:56 +0900

    ThreadLocal

--- src/main/java/com/example/inflearnspringsecurity/form/SampleService.java
+++ src/main/java/com/example/inflearnspringsecurity/form/SampleService.java
@@ -1,5 +1,7 @@
 package com.example.inflearnspringsecurity.form;
 
+import com.example.inflearnspringsecurity.account.Account;
+import com.example.inflearnspringsecurity.account.AccountContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,10 +12,14 @@ import java.util.Collection;
 @Service
 public class SampleService {
     public void dashboard() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object principal = authentication.getPrincipal();
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        Object credentials = authentication.getCredentials();
-        boolean authenticated = authentication.isAuthenticated();
+        Account account = AccountContext.getAccount();
+        System.out.println("================");
+        System.out.println("account.getUsername() = " + account.getUsername());
+        
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        Object principal = authentication.getPrincipal();
+//        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+//        Object credentials = authentication.getCredentials();
+//        boolean authenticated = authentication.isAuthenticated();
     }
 }
