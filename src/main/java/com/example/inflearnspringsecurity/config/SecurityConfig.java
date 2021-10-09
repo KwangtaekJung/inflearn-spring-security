045ab3c4e46503c9d9881f60f10f2bcb82e71f0b
KwangtaekJung <kt79.jung@gmail.com>
2021-10-09 22:10:43 +0900

    커스텀 필터, 타임리프 확장팩, sec 네임스페이스

--- src/main/java/com/example/inflearnspringsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/inflearnspringsecurity/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.inflearnspringsecurity.config;
 
 import com.example.inflearnspringsecurity.account.AccountService;
+import com.example.inflearnspringsecurity.common.LoggingFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
@@ -24,6 +25,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.access.expression.WebExpressionVoter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -62,6 +65,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new LoggingFilter(), WebAsyncManagerIntegrationFilter.class);
+
         http.authorizeRequests()
                 .mvcMatchers("/", "/info", "/account/**", "/signup/**").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
